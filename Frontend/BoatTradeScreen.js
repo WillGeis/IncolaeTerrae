4c4c0a7c49727b79dbc44a993ec8336e774b3a47
WillGeis <willgeister@gmail.com>
2026-04-10 23:44:55 -0500

    CLOSES #5

--- Frontend/BoatTradeScreen.js
+++ Frontend/BoatTradeScreen.js
@@ -163,7 +163,7 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderWidth: 1.5,
     borderRadius: 3,
-    backgroundColor: "transparent",
+    backgroundColor: "transparent", //
   },
   resourceBtnDisabled: {
     opacity: 0.2,
